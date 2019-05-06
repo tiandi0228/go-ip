a8ac087ca368093d8f3219e3729145881b0c23d8
tiandi0228 <tiandi0228@live.com>
2019-05-06 19:25:19 +0800

    fix bug

--- lib/ip.go
+++ lib/ip.go
@@ -12,7 +12,6 @@ import (
 	"github.com/axgle/mahonia"
 	"github.com/parnurzeal/gorequest"
 	"math/rand"
-	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
@@ -32,16 +31,18 @@ func GetIp() {
 		url := "http://ip.jiangxianli.com/?page=" + strconv.Itoa(i)
 		rand.Seed(time.Now().UnixNano())
 		index := rand.Intn(len(userAgents))
-		request, _ := http.NewRequest("GET", url, nil)
-		request.Header.Set("User-Agent", userAgents[index])
 
-		cli1 := &http.Client{}
-		response, err := cli1.Do(request)
+		_, body, errs := gorequest.New().Get(url).Set("User-Agent", UserAgents()[index]).End()
+
+		if errs != nil {
+			fmt.Println(errs)
+		}
+
+		dom, err := goquery.NewDocumentFromReader(strings.NewReader(decoderConvert("utf-8", body)))
+
 		if err != nil {
 			fmt.Println(err)
 		}
-
-		dom, _ := goquery.NewDocumentFromReader(response.Body)
 
 		dom.Find(".table tbody tr").Each(func(i int, context *goquery.Selection) {
 			ss := context.Find("td").Eq(1).Text()
@@ -70,9 +71,17 @@ func GetXiCi() {
 		rand.Seed(time.Now().UnixNano())
 		index := rand.Intn(len(userAgents))
 
-		_, body, _ := gorequest.New().Get(url).Set("User-Agent", UserAgents()[index]).End()
+		_, body, errs := gorequest.New().Get(url).Set("User-Agent", UserAgents()[index]).End()
 
-		dom, _ := goquery.NewDocumentFromReader(strings.NewReader(decoderConvert("utf-8", body)))
+		if errs != nil {
+			fmt.Println(errs)
+		}
+
+		dom, err := goquery.NewDocumentFromReader(strings.NewReader(decoderConvert("utf-8", body)))
+
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		dom.Find(".fl-table tbody tr").Each(func(i int, context *goquery.Selection) {
 			ss := context.Find("td").Eq(1).Text()
@@ -96,9 +105,17 @@ func GetKuaiSu() {
 		rand.Seed(time.Now().UnixNano())
 		index := rand.Intn(len(userAgents))
 
-		_, body, _ := gorequest.New().Get(url).Set("User-Agent", UserAgents()[index]).End()
+		_, body, errs := gorequest.New().Get(url).Set("User-Agent", UserAgents()[index]).End()
 
-		dom, _ := goquery.NewDocumentFromReader(strings.NewReader(decoderConvert("utf-8", body)))
+		if errs != nil {
+			fmt.Println(errs)
+		}
+
+		dom, err := goquery.NewDocumentFromReader(strings.NewReader(decoderConvert("utf-8", body)))
+
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		dom.Find(".table tbody tr").Each(func(i int, context *goquery.Selection) {
 			ss := context.Find("td").Eq(0).Text()
@@ -120,9 +137,17 @@ func GetGouBanJia() {
 	rand.Seed(time.Now().UnixNano())
 	index := rand.Intn(len(userAgents))
 
-	_, body, _ := gorequest.New().Get(url).Set("User-Agent", UserAgents()[index]).End()
+	_, body, errs := gorequest.New().Get(url).Set("User-Agent", UserAgents()[index]).End()
 
-	dom, _ := goquery.NewDocumentFromReader(strings.NewReader(decoderConvert("utf-8", body)))
+	if errs != nil {
+		fmt.Println(errs)
+	}
+
+	dom, err := goquery.NewDocumentFromReader(strings.NewReader(decoderConvert("utf-8", body)))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	dom.Find(".table tbody tr").Each(func(_ int, context *goquery.Selection) {
 		sf, _ := context.Find(".ip").Html()
