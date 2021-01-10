711fceec1fba5c7fff03a87e510c1f637f8b6de9
hongcha <tiandi0228@live.com>
2021-01-10 16:19:57 +0800

    update: 调整抓取方式

--- database/mysql.go
+++ database/mysql.go
@@ -11,7 +11,7 @@ var logger = logrus.New()
 
 func init() {
 	var err error
-	Orm, err = xorm.NewEngine("mysql", "root:Stz#0428!@tcp(127.0.0.1:3306)/ip?charset=utf8")
+	Orm, err = xorm.NewEngine("mysql", "root:Stz#0428!@tcp(sh-cynosdbmysql-grp-lxsrm2i8.sql.tencentcdb.com:27377)/ip?charset=utf8")
 	Orm.ShowSQL(false)
 	if err != nil {
 		logger.Error(err.Error())
